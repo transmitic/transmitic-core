273da1f4821155c2ff83cf9053582440621bf6bc
a <a>
2021-11-21 21:01:38 -0500

    transmitic config init

--- src/crypto.rs
+++ src/crypto.rs
@@ -2,19 +2,20 @@ use ring::{
 	rand,
 	signature::{self, KeyPair},
 };
-use std::vec::Vec;
+use std::{error::Error, vec::Vec};
 extern crate base64;
+//ring::error::Unspecified
 
-pub fn generate_id_pair() -> (Vec<u8>, Vec<u8>) {
+pub fn generate_id_pair() -> Result<(Vec<u8>, Vec<u8>), ring::error::Unspecified> {
 	// Generate a key pair in PKCS#8 (v2) format.
 	let rng = rand::SystemRandom::new();
-	let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng).unwrap();
+	let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng)?;
 
-	let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()).unwrap();
+	let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref())?;
 
 	let public_key_bytes = key_pair.public_key().as_ref();
 
-	return (pkcs8_bytes.as_ref().to_vec(), public_key_bytes.to_vec());
+	return Ok((pkcs8_bytes.as_ref().to_vec(), public_key_bytes.to_vec()));
 }
 
 pub fn get_bytes_from_base64_str(string: &String) -> Vec<u8> {
