194f266e7fef471c1f60a2783722cb4cb54b84a4
a <a>
2021-12-14 00:10:53 -0500

    SingleUploader init

--- src/incoming_uploader.rs
+++ src/incoming_uploader.rs
@@ -1,5 +1,5 @@
 use core::time;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
@@ -13,8 +13,16 @@ pub enum SharingState {
     Internet,
 }
 
+enum MessageUploadManager {
+
+}
+
+enum MessageSingleUploader {
+
+}
+
 pub struct IncomingUploader {
-    sender: Sender<i32>,
+    sender: Sender<MessageUploadManager>,
 }
 
 impl IncomingUploader {
@@ -22,8 +30,8 @@ impl IncomingUploader {
     pub fn new(config: Config) -> IncomingUploader {
 
         let (sx, rx): (
-            Sender<i32>,
-            Receiver<i32>,
+            Sender<MessageUploadManager>,
+            Receiver<MessageUploadManager>,
         ) = mpsc::channel();
 
         thread::spawn(move || {
@@ -39,26 +47,34 @@ impl IncomingUploader {
 
 
 struct UploaderManager {
-    receiver: Receiver<i32>,
+    receiver: Receiver<MessageUploadManager>,
     config: Config,
     sharing_state: SharingState,
+    // TODO once a SingleUplaoder thread is dead, is the reciever dead, and thus can be removed from
+    // this vec on next interation? If not, SingleUploader needs to send a message back around.
+    // 1. Map usize to Senders. SingleUploader gets its ID. At shutdown send to AppAggreator to pop it
+    //  When adding a new one loop through usize until you find available int.
+    single_uploaders: Vec<Sender<MessageSingleUploader>>,
 }
 
 impl UploaderManager {
 
-    pub fn new(receiver: Receiver<i32>, config: Config, sharing_state: SharingState) -> UploaderManager {
+    pub fn new(receiver: Receiver<MessageUploadManager>, config: Config, sharing_state: SharingState) -> UploaderManager {
+
+        let single_uploaders = Vec::new();
 
         return UploaderManager {
             receiver: receiver,
             config,
             sharing_state,
+            single_uploaders,
         }
     }
 
     pub fn run(&mut self) {
         println!("UploaderManager running");
 
-        let mut ip_address = String::new();
+        let mut ip_address;
         loop {
 
             match self.sharing_state {
@@ -85,9 +101,65 @@ impl UploaderManager {
                     todo!()
                 },
             }
+
+            for stream in listener.incoming() {
+                let stream = match stream {
+                    Ok(stream) => {
+                        let (sx, rx): (
+                            Sender<MessageSingleUploader>,
+                            Receiver<MessageSingleUploader>,
+                        ) = mpsc::channel();
+                        self.single_uploaders.push(sx);
+                        let single_config = self.config.clone();
+                        thread::spawn(move || {
+                            let mut single_uploader = SingleUploader::new(rx, single_config, stream);
+                            single_uploader.run();
+                        });
+
+                    },
+                    Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                        thread::sleep(time::Duration::from_secs(1));
+                        continue;
+                    }
+                    Err(e) => {
+                        // TODO
+                        println!("ERROR: Failed initial client connection");
+                        println!("{:?}", e);
+                        continue;
+                    }
+                };
+
+
+            }
             
 
         }
     }
 
+}
+
+
+struct SingleUploader {
+    receiver: Receiver<MessageSingleUploader>,
+    config: Config,
+    stream: TcpStream,
+    nickname: String,
+}
+
+impl SingleUploader {
+
+    pub fn new(receiver: Receiver<MessageSingleUploader>, config: Config, stream: TcpStream) -> SingleUploader {
+
+        let nickname =  String::new();
+        return SingleUploader {
+            receiver,
+            config,
+            stream,
+            nickname,
+        }
+    }
+
+    pub fn run(&mut self) {
+        
+    }
 }
